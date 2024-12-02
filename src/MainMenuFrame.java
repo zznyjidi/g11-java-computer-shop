25192089c0d378cb8c9c9184216cc8c320a6a709
shift.0x7d8 <zixuan.zhao@ms.zzny.fun>
2024-12-01 20:42:16 -0500

    Add ResultFrame and Panels classes

--- src/MainMenuFrame.java
+++ src/MainMenuFrame.java
@@ -9,7 +9,6 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
--- src/ResultComputerInfoPanel.java
+++ src/ResultComputerInfoPanel.java
@@ -1,8 +1,48 @@
 
+import java.awt.Image;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 
-public class ResultComputerInfoPanel {
-    private ImageIcon computerImage;
-    private JLabel computerInfo;
+@SuppressWarnings({ "FieldMayBeFinal", "serial" })
+public class ResultComputerInfoPanel extends JPanel {
+    private ImageIcon computerImageIcon;
+    private JLabel computerImageLabel;
+    private JLabel computerModelLabel;
+    private JLabel computerInfoLabel;
+    private JLabel computerPriceLabel;
+    private JButton moreInfoButton;
+
+    public ResultComputerInfoPanel(String computerName, Image computerImage, String computerInfo) {
+        // Computer Image
+        computerImageIcon = new ImageIcon(
+            // Resize Image 
+            // (From https://stackoverflow.com/questions/6714045/how-to-resize-jlabel-imageicon)
+            computerImage.getScaledInstance(300, 300, java.awt.Image.SCALE_SMOOTH)
+        );
+        computerImageLabel = new JLabel(computerImageIcon);
+        //computerImageLabel.setBounds(0, 0, 300, 300);
+        this.add(computerImageLabel);
+
+        // Computer Name
+        computerModelLabel = new JLabel(computerName);
+        //computerModelLabel.setBounds(320, 0, 300, 20);
+        this.add(computerModelLabel);
+
+        // Computer Info
+        computerInfoLabel = new JLabel(computerInfo);
+        //computerInfoLabel.setBounds(340, 0, 300, 300);
+        this.add(computerInfoLabel);
+    }
+
+    public static void main(String[] args) {
+        JFrame testFrame = new JFrame();
+        testFrame.setVisible(true);
+        testFrame.setSize(1000, 1000);
+        Image computer = new ImageIcon("assets/computers/5.png").getImage();
+        JPanel infoPanel = new ResultComputerInfoPanel("Test Computer", computer, "");
+        testFrame.add(infoPanel);
+    }
 }
