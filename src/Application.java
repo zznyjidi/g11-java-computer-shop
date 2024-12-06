8d7a662a884d266f66ab300fe11306b8d6299619
shift.0x7d8 <zixuan.zhao@ms.zzny.fun>
2024-12-06 13:02:23 -0500

    fix: Application Entry Point

--- src/Application.java
+++ src/Application.java
@@ -1,7 +1,27 @@
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
 
 public class Application {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		FileToArray.readFileToDatabase("data/laptop-info.csv", 20);
+
+        JFrame mainFrame = new JFrame();
+        mainFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        mainFrame.setLayout(null);
+        mainFrame.setVisible(true);
+        mainFrame.setSize(1200, 610);
+
+        Database.mainFrame = mainFrame;
+
+        MenuBar menuBar = new MenuBar();
+        menuBar.setBounds(0, 0, 1200, 60);
+        mainFrame.add(menuBar);
+
+		menuBar.changePanel(new MainMenuFrame());
+
+        // bugfix: Force Rerender Window to Make Panel shows up
+        mainFrame.revalidate();
+        mainFrame.repaint();
 }
 }
